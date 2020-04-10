720f104781a313db51b6ab0b7622214186700e7a
Ishmum Jawad Khan <ishmumkhan@gmail.com>
2020-04-10 12:00:54 +0600

    [feature] patient activity skeleton

---
+++ app/src/main/java/com/cronocode/materialloginpage/PatientActivity.kt
@@ -0,0 +1,12 @@
+package com.cronocode.materialloginpage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PatientActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patient)
+    }
+}
