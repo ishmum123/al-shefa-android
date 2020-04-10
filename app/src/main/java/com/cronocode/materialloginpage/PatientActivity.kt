313a73a9fc25e3792742fed58319344fde3c2f20
Ishmum Jawad Khan <ishmumkhan@gmail.com>
2020-04-10 12:57:20 +0600

    [feature] find doctor activity skeleton

--- app/src/main/java/com/cronocode/materialloginpage/PatientActivity.kt
+++ app/src/main/java/com/cronocode/materialloginpage/PatientActivity.kt
@@ -1,12 +1,20 @@
 package com.cronocode.materialloginpage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class PatientActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patient)
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    fun findDoctor(view: View) {
+        val intent = Intent(this, FindDoctorActivity::class.java)
+        startActivity(intent)
     }
 }
